a33536e62aad9c244d2574724b71cb18d57e3985
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-19 16:58:18 +0200

    starting structure

---
+++ index.js
@@ -0,0 +1,13 @@
+//const imgArray = [
+    //"01.jpg",
+    //"02.jpg",
+    //"03.jpg",
+    //"04.jpg",
+    //"05.jpg"
+//]
+
+//const itemsDom = document.querySelector ('.items');
+
+//for (let i = 0; i < imgArray.length ; i++) {
+  //  itemsDom.innerHTML += `<div class="item"><img class=img-slide src="${imgArray[i]}"/></div>`
+//}
